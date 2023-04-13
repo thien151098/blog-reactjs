c2c5796fd8665103e2a248b24a21dc2df9f8e11d
thien151098 <126788776+thien151098@users.noreply.github.com>
2023-04-13 19:10:09 +0700

    c3

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/pages/Home';
+import Blog from './components/pages/Blog';
+import Blog2 from './components/pages/Blog2';
+import { publicRoutes } from './routes';
+
 function App() {
     return (
-        <div className="App">
-            <h1>App</h1>
-        </div>
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Page = route.component;
+
+                        return <Route key={index} path={route.path} element={<Page />} />;
+                    })}
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
